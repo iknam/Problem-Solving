368f0f5830035853f2b2695f25e608f1f937f8e9
mondoqed <mondoqed@gmail.com>
2015-08-04 18:52:26 +0900

    Update ATM.cpp

--- Dovelet/Unsolved/Selection/ATM.cpp
+++ Dovelet/Unsolved/Selection/ATM.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-
-
+    int X;
+    double Y;
+    
+    cin >> X >> Y;
+    cout << fixed;
+    
+    if (X % 5 != 0 || X >= (int)Y)
+    {
+        cout << setprecision(2) << Y;
+        return 0;
+    }
+    
+    cout << setprecision(2) << Y - (double)X - 0.5;
+    
     return 0;
 }
-
