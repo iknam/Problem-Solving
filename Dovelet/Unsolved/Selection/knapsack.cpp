02bc960bbd079c7251e78af2fd2e68706208902f
mondoqed <mondoqed@gmail.com>
2015-08-09 16:05:14 +0900

    Update knapsack.cpp

--- Dovelet/Unsolved/Selection/knapsack.cpp
+++ Dovelet/Unsolved/Selection/knapsack.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-	int i, j, n, gemnum;
+	int i, j, n, gemnum, weight = 0;
 	double ans = 0;
 	vector< pair<int, int> > gem;
 	vector< pair<int, int> > prior;
@@ -22,10 +22,12 @@ int main()
 		cin >> gem[i].first >> gem[i].second;
 		prior[i].first = gem[i].second / gem[i].first; //prior
 		prior[i].second = gem[i].first;
+		//cout << prior[i].second << endl;
 	}
 
-	sort(prior.begin(), prior.end());
-
+	sort(prior.begin(), prior.end(), greater< pair<int, int> >());
+	//cout << prior[0].first << " " << prior[0].second << endl;
+	
 	for (i = 0; i < gemnum; i++)
 	{
 		for (j = 0; j < gemnum; j++)
@@ -33,7 +35,14 @@ int main()
 			if (prior[i].second == gem[j].first)
 			{
 				ans += (double)(gem[j].second);
-				if (accumulate(
+				cout << ans << endl;
+				weight += gem[i].first;
+				if (weight > n)
+				{
+					weight - n
+					ans -= 
+				}
+				//if (accumulate(
 			}
 		}
 	}
