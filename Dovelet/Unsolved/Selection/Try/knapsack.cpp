a122f6d590896982b5dee1a55f6739e70b6ac88c
Nam il kwon <mondoqed@gmail.com>
2015-08-09 18:38:55 +0900

    t

--- Dovelet/Unsolved/Selection/Try/knapsack.cpp
+++ Dovelet/Unsolved/Selection/Try/knapsack.cpp
@@ -9,8 +9,8 @@ int main()
 {
 	int i, j, n, gemnum, weight = 0;
 	double ans = 0;
-	vector< pair<int, int> > gem;
-	vector< pair<int, int> > prior;
+	vector< pair<double, double> > gem;
+	vector< pair<double, double> > prior;
 
 	cin >> n >> gemnum;
 
@@ -34,22 +34,25 @@ int main()
 		{
 			if (prior[i].second == gem[j].first)
 			{
-				ans += (double)(gem[j].second);
-				cout << ans << endl;
+				ans += (gem[j].second);
+				// cout << ans << endl;
 				weight += gem[i].first;
 				if (weight > n)
 				{
-					weight - n
-					ans -= 
+					weight -= n;
+					//cout << gem[i-1].second << " " << gem[i-1].first << " " << weight << " ";
+					ans -= gem[i - 1].second / (gem[i - 1].first / weight);
+					// gem[i].first / (gem[i].second / (weight - n));
+					//ans -= 
+					cout << fixed;
+					cout << setprecision(2) << ans;
+					return 0;
 				}
 				//if (accumulate(
 			}
 		}
 	}
 	
-	cout << fixed;
-	cout << setprecision(2) << ans;
-
     return 0;
 }
 
---
+++ Hackerrank/Unsolved/Selection/fibonacci-modified.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int A, B, N;
+
+	cin >> A >> B >> N;
+
+
+
+    return 0;
+}
+
