b9f8706d2f4eef39f38030f75647ff8ea8abc3d4
mondoqed <mondoqed@gmail.com>
2015-08-12 19:38:15 +0900

    Update REPEATLESS.cpp

--- Algospot/Unsolved/Selection/REPEATLESS.cpp
+++ Algospot/Unsolved/Selection/REPEATLESS.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+	string str;
+	int num;
+	while (num != 0)
+	{
+		//cin >> num;
+		//cout << num;
+	}
+	
+    cin >> num;
+    
+	str = to_string(num);
+	
+    cout << str;
+	sort(str.begin(), str.end());
+	
+	do
+	{
+		cout << str[0] << str[1] << str[2];
+	} while (next_permutation(str.begin(), str.end()));
+	
+	return 0;
+}
