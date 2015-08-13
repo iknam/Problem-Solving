134c9f9e738bf8812c862ffa39784e7c5d5e85d1
mondoqed <mondoqed@gmail.com>
2015-08-13 18:57:41 +0900

    Update python-mutations.py

--- Hrpy/Unsolved/Selection/python-mutations.py
+++ Hrpy/Unsolved/Selection/python-mutations.py
@@ -1,10 +1,8 @@
 S = str(input())
 ic = str(input())
-if len(ic) > 3:
-    i = int(
-else:
-    i = int(ic[0])
-    c = str(ic[2])
+
+i = int(ic[:len(ic) - 2])
+c = str(ic[len(ic)])
 
 ans = str("")
 ans = S[0:i]
