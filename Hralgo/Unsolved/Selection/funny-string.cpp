e9098372a9d925b4e22d5b8a4a85419cd33da2db
mondoqed <mondoqed@gmail.com>
2015-08-14 13:00:22 +0900

    Update funny-string.cpp

--- Hralgo/Unsolved/Selection/funny-string.cpp
+++ Hralgo/Unsolved/Selection/funny-string.cpp
@@ -1,3 +1,37 @@
-#include <ok>
-new
-old
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int i, T;
+	
+	cin >> T;
+	
+	string *S = new string[T];
+	string *R = new string[T];
+	
+	for (i = 0; i < T; i++)
+	{
+		cin >> S[i];
+		//cout << S[i];
+	}
+	
+	S.reverse_copy(S.begin(), S.end(), R);
+	
+	cout
+	
+	for (i = 0; i < T; i++)
+	{
+	if (abs(S[i][0]-S[i][1]) == abs(S[i][2] - S[i][3]))
+	{
+		cout << "Funny" << endl;
+	}
+	else if (abs(S[i][0]-S[i][1]) != abs(S[i][2] - S[i][3]))
+	{
+		cout << "Not Funny" << endl;
+	}
+	}
+}
