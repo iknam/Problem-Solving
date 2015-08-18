194246891801164a4b3a5ed87afbc2485d256876
mondoqed <mondoqed@gmail.com>
2015-08-18 12:53:47 +0900

    Update valid-pan-format.py

--- Regex/Unsolved/Selection/valid-pan-format.py
+++ Regex/Unsolved/Selection/valid-pan-format.py
@@ -1,17 +1,15 @@
 import re
 
-PAN = []
+PAN = list()
 
 N = int(input())
 
 for i in range(0, N):
     PAN.append(str(input()))
-    if len(PAN[i]) != 10:
-        print("yeah")
-    char_search = bool(re.search(r"[a-z] +5", PAN[i]))
-    int_search = bool(re.search(r"[0-9] + 4", PAN[i]))
-    print(char_search);
-    print(int_search)
+    char_search = bool(re.search(r"[A-Z]{5}", PAN[i]))
+    int_search = bool(re.search(r"[0-9]{4}", PAN[i]))
+#    print(char_search)
+#    print(int_search)
     if char_search == True and int_search == True:
         print("YES")
     else:
